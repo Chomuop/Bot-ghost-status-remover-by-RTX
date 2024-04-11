1e82aa93138f01d1f26b64d496b661287fe21c78
Chomuop <160112992+Chomuop@users.noreply.github.com>
2024-04-11 18:02:50 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to /help","Listening to Devil"];
 
 
 let currentIndex = 0;
